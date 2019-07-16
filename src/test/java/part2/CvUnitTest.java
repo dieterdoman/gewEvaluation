a03dcb06dd5509c7285d9edf46658b83070521f8
Dieter Doman <dieter@jemstep.com>
2019-07-16 10:00:18 +0200

    Added test for part 2.

--- src/test/java/part2/CvUnitTest.java
+++ src/test/java/part2/CvUnitTest.java
@@ -2,13 +2,28 @@ package part2;
 
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class CvUnitTest {
 
     @Test
-    public void testDeepVsShallowCopy()
-    {
-
+    public void testDeepVsShallowCopy() throws ParseException {
+        PersonalDetails personalDetails = new PersonalDetails("Dieter", "Doman");
+        List<WorkItem> workHistory = new ArrayList<>();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        workHistory.add(new WorkItem("Test Company 1", "Software Engineer", dateFormat.parse("2014-02-11"), dateFormat.parse("2015-02-11")));
+        workHistory.add(new WorkItem("Test Company 2", "Software Engineer", dateFormat.parse("2015-02-12"), dateFormat.parse("2016-02-11")));
+        List<Qualification> qualifications = new ArrayList<>();
+        qualifications.add(new Qualification("BscIT", "UP", dateFormat.parse("2013-02-11")));
+        Cv cv = new Cv(personalDetails, workHistory, qualifications);
+        assertNotSame(personalDetails, cv.getPersonalDetails());
+        assertNotSame(workHistory, cv.getWorkHistory());
+        assertNotSame(qualifications, cv.getQualifications());
     }
 }
